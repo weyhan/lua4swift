3ad660ce6a484d8f59670a9bcaeb10449726ed6b
Steven Degutis <sbdegutis@gmail.com>
2014-12-13 12:18:46 -0600

    Initial Commit

---
+++ DIY Window Manager/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  DIY Window Manager
+//
+//  Created by Steven Degutis on 12/13/14.
+//  Copyright (c) 2014 Tiny Robot Software. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
