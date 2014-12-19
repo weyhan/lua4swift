315bb86edb5107f93c3e9202f5cfa37b3d9b9afa
Steven Degutis <sbdegutis@gmail.com>
2014-12-19 12:32:35 -0600

    Nesting type for fun and profit.

--- DIY Window Manager/LuaHotkey.swift
+++ DIY Window Manager/LuaHotkey.swift
@@ -1,61 +1,65 @@
 import Foundation
 
 
-class LuaHotkey: LuaMetatableOwner {
-    let fn: Int
-    let hotkey: Hotkey
+class API {
     
-    class var metatableName: String { return "Hotkey" }
-    
-    init(fn: Int, hotkey: Hotkey) {
-        self.fn = fn
-        self.hotkey = hotkey
-    }
-    
-    func call(L: Lua) {
-        L.rawGet(tablePosition: Lua.RegistryIndex, index: fn)
-        L.call(arguments: 1, returnValues: 0)
-    }
-    
-    class func pushLibrary(L: Lua) {
-        L.pushTable()
+    class Hotkey: LuaMetatableOwner {
+        let fn: Int
+        let hotkey: DIY_Window_Manager.Hotkey
         
-        L.pushMetatable("Hotkey") {
-            L.pushMetaMethod(.EQ({ (a: LuaHotkey, b: LuaHotkey) in
-                return a.fn == b.fn
-            }))
-            
-            L.pushMetaMethod(.GC({ (L, o: LuaHotkey) in
-                o.hotkey.disable()
-                L.unref(Lua.RegistryIndex, o.fn)
-            }))
+        class var metatableName: String { return "Hotkey" }
+        
+        init(fn: Int, hotkey: DIY_Window_Manager.Hotkey) {
+            self.fn = fn
+            self.hotkey = hotkey
         }
         
-        L.pushMethod("bind") { L in
-            L.checkArgs(.String, .Table, .Function, .None)
-            let key = L.getString(1)!
-            let mods = L.getTable(2)!
-            L.pushFromStack(3)
+        func call(L: Lua) {
+            L.rawGet(tablePosition: Lua.RegistryIndex, index: fn)
+            L.call(arguments: 1, returnValues: 0)
+        }
+        
+        class func pushLibrary(L: Lua) {
+            L.pushTable()
             
-            var modStrings = [String]()
-            for (_, mod) in mods {
-                switch mod {
-                case let .String(s): modStrings.append(s);
-                default: break
-                }
+            L.pushMetatable("Hotkey") {
+                L.pushMetaMethod(.EQ({ (a: Hotkey, b: Hotkey) in
+                    return a.fn == b.fn
+                }))
+                
+                L.pushMetaMethod(.GC({ (L, o: Hotkey) in
+                    o.hotkey.disable()
+                    L.unref(Lua.RegistryIndex, o.fn)
+                }))
             }
             
-            let hotkey = Hotkey(key: key, mods: modStrings, downFn: {}, upFn: nil)
-            hotkey.enable()
+            L.pushMethod("bind") { L in
+                L.checkArgs(.String, .Table, .Function, .None)
+                let key = L.getString(1)!
+                let mods = L.getTable(2)!
+                L.pushFromStack(3)
+                
+                var modStrings = [String]()
+                for (_, mod) in mods {
+                    switch mod {
+                    case let .String(s): modStrings.append(s);
+                    default: break
+                    }
+                }
+                
+                let hotkey = DIY_Window_Manager.Hotkey(key: key, mods: modStrings, downFn: {}, upFn: nil)
+                hotkey.enable()
+                
+                let i = L.ref(Lua.RegistryIndex)
+                L.pushMetaUserdata(Hotkey(fn: i, hotkey: hotkey))
+                
+                return 1
+            }
             
-            let i = L.ref(Lua.RegistryIndex)
-            L.pushMetaUserdata(LuaHotkey(fn: i, hotkey: hotkey))
-            
-            return 1
+            // Hotkey.__index = Hotkey
+            L.pushFromStack(-1)
+            L.setField("__index", table: -2)
         }
-        
-        // Hotkey.__index = Hotkey
-        L.pushFromStack(-1)
-        L.setField("__index", table: -2)
     }
+    
 }
