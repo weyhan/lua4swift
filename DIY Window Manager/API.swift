f9d122777afce049131e3db6297c0b34e7971451
Steven Degutis <sbdegutis@gmail.com>
2014-12-20 16:42:57 -0600

    Renaming DIY_Window_Manager.Hotkey -> Carbon.Hotkey

--- DIY Window Manager/API.swift
+++ DIY Window Manager/API.swift
@@ -4,7 +4,7 @@ class API {
     
     final class Hotkey: LuaLibrary {
         let fn: Int
-        let hotkey: DIY_Window_Manager.Hotkey
+        let hotkey: Carbon.Hotkey
         
         class var metatableName: String { return "Hotkey" }
         
@@ -16,14 +16,9 @@ class API {
             return L.getUserdata(position) as? Hotkey
         }
         
-        init(fn: Int, hotkey: DIY_Window_Manager.Hotkey) {
+        init(fn: Int, hotkey: Carbon.Hotkey) {
             self.fn = fn
             self.hotkey = hotkey
-        }
-        
-        func call(L: Lua) {
-            L.rawGet(tablePosition: Lua.RegistryIndex, index: fn)
-            L.call(arguments: 1, returnValues: 0)
         }
         
         func enable(L: Lua) -> [LuaValue] {
@@ -39,14 +34,18 @@ class API {
         class func bind(L: Lua) -> [LuaValue] {
             let key = String.fromLua(L, at: 1)!
             let mods = Lua.TableBox.fromLua(L, at: 2)!.t
-            L.pushFromStack(3)
-            
             let modStrings = mods.map{$1 as? String}.filter{$0 != nil}.map{$0!}
             
-            let hotkey = DIY_Window_Manager.Hotkey(key: key, mods: modStrings, downFn: {}, upFn: nil)
-            hotkey.enable()
-            
+            L.pushFromStack(3)
             let i = L.ref(Lua.RegistryIndex)
+            
+            let downFn: Carbon.Hotkey.Callback = {
+                L.rawGet(tablePosition: Lua.RegistryIndex, index: i)
+                L.call(arguments: 1, returnValues: 0)
+            }
+            
+            let hotkey = Carbon.Hotkey(key: key, mods: modStrings, downFn: downFn, upFn: nil)
+            hotkey.enable()
             
             return [Hotkey(fn: i, hotkey: hotkey)]
         }
