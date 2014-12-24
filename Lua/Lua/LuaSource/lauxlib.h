13598c9e23ca0b3c8bf29447da22f6ab1bf768fd
Steven Degutis <sbdegutis@gmail.com>
2014-12-24 00:51:07 -0600

    Adding function to Lua itself.

--- Lua/Lua/LuaSource/lauxlib.h
+++ Lua/Lua/LuaSource/lauxlib.h
@@ -36,6 +36,7 @@ LUALIB_API int (luaL_getmetafield) (lua_State *L, int obj, const char *e);
 LUALIB_API int (luaL_callmeta) (lua_State *L, int obj, const char *e);
 LUALIB_API const char *(luaL_tolstring) (lua_State *L, int idx, size_t *len);
 LUALIB_API int (luaL_argerror) (lua_State *L, int arg, const char *extramsg);
+LUALIB_API int (luaL_typeerror) (lua_State *L, int arg, const char *tname);
 LUALIB_API const char *(luaL_checklstring) (lua_State *L, int arg,
                                                           size_t *l);
 LUALIB_API const char *(luaL_optlstring) (lua_State *L, int arg,
