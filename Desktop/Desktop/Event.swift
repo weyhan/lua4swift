f243006c32b2d8bd4f754791daabc95d13073c49
Steven Degutis <sbdegutis@gmail.com>
2015-01-02 11:49:03 -0600

    Revert "Getting ready to try another Events interface."
    This reverts commit 363014ab8c3dc483b4354df7ffc68cbc5b7c02a3.
    
    Meh. What we have is good enough.

--- Desktop/Desktop/Event.swift
+++ Desktop/Desktop/Event.swift
@@ -1,7 +1,5 @@
 import Foundation
 
-
-
 public class DesktopEventHandler {
     
     public enum Event {
@@ -69,89 +67,89 @@ public class DesktopEventHandler {
     
 }
 
-//public class AppEventHandler {
-//    
-//    public enum Event {
-//        case WindowCreated(Window -> Void)
-//        case ElementDestroyed(Window -> Void)
-//        case WindowMoved(Window -> Void)
-//        case WindowResized(Window -> Void)
-//        case WindowMiniaturized(Window -> Void)
-//        case WindowDeminiaturized(Window -> Void)
-//        case ApplicationHidden(App -> Void)
-//        case ApplicationShown(App -> Void)
-//        case FocusedWindowChanged(Window -> Void)
-//        case ApplicationActivated(App -> Void)
-//        case MainWindowChanged(Window? -> Void)
-//        
-//        private func name() -> String {
-//            switch self {
-//            case WindowCreated:        return "AXWindowCreated"
-//            case ElementDestroyed:     return "AXUIElementDestroyed"
-//            case WindowMoved:          return "AXWindowMoved"
-//            case WindowResized:        return "AXWindowResized"
-//            case WindowMiniaturized:   return "AXWindowMiniaturized"
-//            case WindowDeminiaturized: return "AXWindowDeminiaturized"
-//            case ApplicationHidden:    return "AXApplicationHidden"
-//            case ApplicationShown:     return "AXApplicationShown"
-//            case FocusedWindowChanged: return "AXFocusedWindowChanged"
-//            case ApplicationActivated: return "AXApplicationActivated"
-//            case MainWindowChanged:    return "AXMainWindowChanged"
-//            }
-//        }
-//        
-//        private func call(element: AXUIElement!) {
-//            switch self {
-//            case let WindowCreated(fn):        fn(Window(element))
-//            case let ElementDestroyed(fn):     fn(Window(element))
-//            case let WindowMoved(fn):          fn(Window(element))
-//            case let WindowResized(fn):        fn(Window(element))
-//            case let WindowMiniaturized(fn):   fn(Window(element))
-//            case let WindowDeminiaturized(fn): fn(Window(element))
-//            case let ApplicationHidden(fn):    fn(App(element))
-//            case let ApplicationShown(fn):     fn(App(element))
-//            case let FocusedWindowChanged(fn): fn(Window(element))
-//            case let ApplicationActivated(fn): fn(App(element))
-//            case let MainWindowChanged(fn):    fn(Window(element))
-//            }
-//        }
-//    }
-//    
-//    private var observer: AXObserver?
-//    private let fn: AXUIElement! -> Void
-//    public let app: App
-//    public let event: Event
-//    
-//    public init?(app: App, event: Event) {
-//        self.app = app
-//        self.event = event
-//        fn = { event.call($0) }
-//    }
-//    
-//    public func enable() -> String? {
-//        if observer != nil { return "Already enabled." }
-//        
-//        var ob: Unmanaged<AXObserver>?
-//        let result = AXObserverCreate(app.pid, SDegutisObserverCallbackTrampoline(), &ob)
-//        if result != AXError(kAXErrorSuccess) { return "AXObserverCreate failed: code \(result)" }
-//        if ob == nil { return "AXObserverCreate didn't do its job right" }
-//        observer = ob!.takeRetainedValue()
-//        
-//        AXObserverAddNotification(observer, app.element, event.name(), SDegutisVoidStarifyBlock(fn))
-//        CFRunLoopAddSource(CFRunLoopGetMain(), AXObserverGetRunLoopSource(observer).takeUnretainedValue(), kCFRunLoopDefaultMode)
-//        
-//        return nil
-//    }
-//    
-//    public func disable() {
-//        if observer == nil { return }
-//        CFRunLoopRemoveSource(CFRunLoopGetMain(), AXObserverGetRunLoopSource(observer).takeUnretainedValue(), kCFRunLoopDefaultMode)
-//        AXObserverRemoveNotification(observer, app.element, event.name())
-//        observer = nil
-//    }
-//    
-//    deinit {
-//        disable()
-//    }
-//    
-//}
+public class AppEventHandler {
+    
+    public enum Event {
+        case WindowCreated(Window -> Void)
+        case ElementDestroyed(Window -> Void)
+        case WindowMoved(Window -> Void)
+        case WindowResized(Window -> Void)
+        case WindowMiniaturized(Window -> Void)
+        case WindowDeminiaturized(Window -> Void)
+        case ApplicationHidden(App -> Void)
+        case ApplicationShown(App -> Void)
+        case FocusedWindowChanged(Window -> Void)
+        case ApplicationActivated(App -> Void)
+        case MainWindowChanged(Window? -> Void)
+        
+        private func name() -> String {
+            switch self {
+            case WindowCreated:        return "AXWindowCreated"
+            case ElementDestroyed:     return "AXUIElementDestroyed"
+            case WindowMoved:          return "AXWindowMoved"
+            case WindowResized:        return "AXWindowResized"
+            case WindowMiniaturized:   return "AXWindowMiniaturized"
+            case WindowDeminiaturized: return "AXWindowDeminiaturized"
+            case ApplicationHidden:    return "AXApplicationHidden"
+            case ApplicationShown:     return "AXApplicationShown"
+            case FocusedWindowChanged: return "AXFocusedWindowChanged"
+            case ApplicationActivated: return "AXApplicationActivated"
+            case MainWindowChanged:    return "AXMainWindowChanged"
+            }
+        }
+        
+        private func call(element: AXUIElement!) {
+            switch self {
+            case let WindowCreated(fn):        fn(Window(element))
+            case let ElementDestroyed(fn):     fn(Window(element))
+            case let WindowMoved(fn):          fn(Window(element))
+            case let WindowResized(fn):        fn(Window(element))
+            case let WindowMiniaturized(fn):   fn(Window(element))
+            case let WindowDeminiaturized(fn): fn(Window(element))
+            case let ApplicationHidden(fn):    fn(App(element))
+            case let ApplicationShown(fn):     fn(App(element))
+            case let FocusedWindowChanged(fn): fn(Window(element))
+            case let ApplicationActivated(fn): fn(App(element))
+            case let MainWindowChanged(fn):    fn(Window(element))
+            }
+        }
+    }
+    
+    private var observer: AXObserver?
+    private let fn: AXUIElement! -> Void
+    public let app: App
+    public let event: Event
+    
+    public init?(app: App, event: Event) {
+        self.app = app
+        self.event = event
+        fn = { event.call($0) }
+    }
+    
+    public func enable() -> String? {
+        if observer != nil { return "Already enabled." }
+        
+        var ob: Unmanaged<AXObserver>?
+        let result = AXObserverCreate(app.pid, SDegutisObserverCallbackTrampoline(), &ob)
+        if result != AXError(kAXErrorSuccess) { return "AXObserverCreate failed: code \(result)" }
+        if ob == nil { return "AXObserverCreate didn't do its job right" }
+        observer = ob!.takeRetainedValue()
+        
+        AXObserverAddNotification(observer, app.element, event.name(), SDegutisVoidStarifyBlock(fn))
+        CFRunLoopAddSource(CFRunLoopGetMain(), AXObserverGetRunLoopSource(observer).takeUnretainedValue(), kCFRunLoopDefaultMode)
+        
+        return nil
+    }
+    
+    public func disable() {
+        if observer == nil { return }
+        CFRunLoopRemoveSource(CFRunLoopGetMain(), AXObserverGetRunLoopSource(observer).takeUnretainedValue(), kCFRunLoopDefaultMode)
+        AXObserverRemoveNotification(observer, app.element, event.name())
+        observer = nil
+    }
+    
+    deinit {
+        disable()
+    }
+    
+}
